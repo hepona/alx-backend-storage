fb8c23157d86cdd73d88ec32c67181a6d920a7a1
hepona <hepona10@gmail.com>
2024-05-09 14:16:58 +0100

    1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- create table users, again
+CREATE TABLE
+    IF NOT EXISTS users (
+        id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        email varchar(255) UNIQUE NOT NULL,
+        name varchar(255),
+        country ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    );

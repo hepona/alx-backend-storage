f8047ddfde3d64d3d586c9834b58245adc2d39d6
hepona <hepona10@gmail.com>
2024-05-09 13:55:10 +0100

    test 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
 -- Create table users
-CREATE TABLE IF NOT EXISTS users {
-    id int PRIMARY KEY NOT NULL ,
-    email varchar(255) UNIQUE NOT NULL,
-    name varchar(255)
-}
+CREATE TABLE
+    IF NOT EXISTS users (
+        id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        email varchar(255) UNIQUE NOT NULL,
+        name varchar(255)
+    );

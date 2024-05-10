6a2c407c427e925ea4e468f1791ebd337a700b5f
hepona <hepona10@gmail.com>
2024-05-10 21:29:39 +0100

    6-add bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,5 +1,16 @@
 -- stored procedure AddBonus that adds a new correction for a student.
-CREATE PROCEDURE AddBonus  @user_id, @project_name ,@score
-AS
-INSERT INTO corrections (user_id, project_name, score)
-VALUES (@user_id, @project_name, @score)
+DELIMITER //
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT )
+BEGIN
+DECLARE project_id INT ;
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name)
+        VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
+    END if;
+
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, project_id ,score);
+END //
+DELIMITER ;

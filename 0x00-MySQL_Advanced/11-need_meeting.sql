c5f45bd4a7b79bda4e154a83f1613bbca89abe87
hepona <hepona10@gmail.com>
2024-05-11 01:13:30 +0100

    fix 11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,4 +2,4 @@
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
     SELECT name FROM students
-    WHERE score < 80 AND (last_meeting IS NULL OR DATEDIFF(CURDATE() - last_meeting)>30);
+    WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));

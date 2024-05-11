52ea88ee7e56a1ad2277d7f3aaaa3f72d3e14077
hepona <hepona10@gmail.com>
2024-05-11 22:21:16 +0100

    advanced task 12

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,10 +1,21 @@
 -- stored procedure that computes and store the average weighted score for a student.
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+
 DELIMITER //
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
 BEGIN
     UPDATE users
-    SET average_score = (SELECT SUM(corrections.score*projects.weight)/SUM(projects.weight) FROM corrections, projects WHERE corrections.user_id = user_id)
-    WHERE id = user_id;
+    SET
+        average_score = (
+            SELECT
+                IF(SUM(corrections.score * projects.weight) > 0, SUM(corrections.score * projects.weight) / SUM(projects.weight), 0)
+            FROM
+                corrections,
+                projects
+            WHERE
+                corrections.user_id = user_id
+        )
+    WHERE
+        id = user_id;
 END //
-DELIMITER ;
+DELIMITER;

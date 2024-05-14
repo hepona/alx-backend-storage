a87183c4732853ee2f4b04c9eeacd88c5983e512
hepona <hepona10@gmail.com>
2024-05-14 13:27:04 +0100

    8+9

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" list all doc in a collection"""
+
+
+def list_all(mongo_collection):
+    """list all doc in collection"""
+    return list(mongo_collection.find())
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" insert school """
+def insert_school(mongo_collection, **kwargs):
+    """ insert new doc to collection school"""
+    return mongo_collection.insert_one(kwargs)

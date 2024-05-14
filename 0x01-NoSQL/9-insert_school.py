1f30fd8b039a9361dccfecbdcfbf7955bab3a702
hepona <hepona10@gmail.com>
2024-05-14 13:44:13 +0100

    6+8

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -2,4 +2,4 @@
 """ insert school """
 def insert_school(mongo_collection, **kwargs):
     """ insert new doc to collection school"""
-    return mongo_collection.insert_one(kwargs)
+    return mongo_collection.insert_one(kwargs).insert_id

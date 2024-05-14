1733728b3ab5492e9e6f2d82130e38d952d0040c
hepona <hepona10@gmail.com>
2024-05-14 16:31:55 +0100

    9+10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,5 @@
 #!/usr/bin/env python3
+""" update"""
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document based on the name"""
+    return mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """ insert school """
+
+
 def insert_school(mongo_collection, **kwargs):
-    """ insert new doc to collection school"""
-    return mongo_collection.insert_one(kwargs).insert_id
+    """insert new doc to collection school"""
+    return mongo_collection.insert_one(kwargs).inserted_id
